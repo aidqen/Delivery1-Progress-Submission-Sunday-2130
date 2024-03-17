215d72d29ca3e1c05bc3ab6daeb7e0e4e23af2e5
aidqen <idanmarkin8@gmail.com>
2024-03-17 14:36:24 +0200

    Implement renderMeme() function

--- js/controllers/meme-edit.controller.js
+++ js/controllers/meme-edit.controller.js
@@ -15,3 +15,26 @@ function resizeCanvas() {
   // Changing the canvas dimension clears the canvas
   gElCanvas.width = elContainer.clientWidth
 }
+
+function renderMeme() {
+  var txtSettings = getLineText()
+  var {txt, size, color} = txtSettings.lines[0]
+  var x = '45'
+  var y = '45'
+
+  // gCtx.lineWidth = 2
+  gCtx.strokeStyle = color
+
+  gCtx.fillStyle = 'lightsteelblue'
+
+  gCtx.font = `${size}px Arial'`
+  gCtx.textAlign = 'center'
+  gCtx.textBaseline = 'middle'
+
+  gCtx.fillText(txt, x, y)
+  gCtx.strokeText(txt, x, y)
+}
+
+function changeText(elTxtInput) {
+  setLineText(elTxtInput.value)
+}
--- js/services/meme.service.js
+++ js/services/meme.service.js
@@ -2,78 +2,89 @@
 var gId = 1
 const gMemes = []
 var gMeme = {
-    selectedImgId: 0,
-    selectedLineIdx: 0,
-    lines: [
-        {
-            txt: 'I sometimes eat falafel',
-            size: 20,
-            color: 'red',
-        }
-    ]
+  selectedImgId: 0,
+  selectedLineIdx: 0,
+  lines: [
+    {
+      txt: 'some random text',
+      size: '20',
+      color: 'white',
+    },
+  ],
 }
 
 function createMemes() {
-    for (var i = 1 ; i < 18 ; i++) {
-        gMemes.push(getMeme(i))
-    }
-    saveToStorage('memesDB',gMemes)
+  for (var i = 1; i < 18; i++) {
+    gMemes.push(getMeme(i))
+  }
+  saveToStorage('memesDB', gMemes)
 }
 
 function getMemes() {
-    var memes = gMemes
-    console.log(memes);
-    return memes
+  var memes = gMemes
+  console.log(memes)
+  return memes
 }
 
 function getMeme(id) {
-    const meme = {
-        txt: '',
-        id,
-        keywords: []
-    }
-    return meme
+  const meme = {
+    txt: '',
+    id,
+    keywords: [],
+  }
+  return meme
 }
 
 function addKeywords() {
-    gMemes[0].keywords.push('men', 'funny')
-    gMemes[1].keywords.push('animal', 'smile')
-    gMemes[2].keywords.push('animal', 'funny','smile')
-    gMemes[3].keywords.push('animal')
-    gMemes[4].keywords.push('men', 'funny')
-    gMemes[5].keywords.push('men', 'funny')
-    gMemes[6].keywords.push('men', 'funny')
-    gMemes[7].keywords.push('men', 'funny')
-    gMemes[8].keywords.push('men', 'funny')
-    gMemes[9].keywords.push('men', 'funny')
-    gMemes[10].keywords.push('men', 'funny')
-    gMemes[11].keywords.push('men', 'funny')
-    gMemes[12].keywords.push('men', 'funny')
-    gMemes[13].keywords.push('men', 'funny')
-    gMemes[14].keywords.push('men', 'funny')
-    gMemes[15].keywords.push('men', 'funny')
-    gMemes[16].keywords.push('men', 'funny')
-    gMemes[17].keywords.push('men', 'funny')
+  gMemes[0].keywords.push('men', 'funny')
+  gMemes[1].keywords.push('animal', 'smile')
+  gMemes[2].keywords.push('animal', 'funny', 'smile')
+  gMemes[3].keywords.push('animal')
+  gMemes[4].keywords.push('men', 'funny')
+  gMemes[5].keywords.push('men', 'funny')
+  gMemes[6].keywords.push('men', 'funny')
+  gMemes[7].keywords.push('men', 'funny')
+  gMemes[8].keywords.push('men', 'funny')
+  gMemes[9].keywords.push('men', 'funny')
+  gMemes[10].keywords.push('men', 'funny')
+  gMemes[11].keywords.push('men', 'funny')
+  gMemes[12].keywords.push('men', 'funny')
+  gMemes[13].keywords.push('men', 'funny')
+  gMemes[14].keywords.push('men', 'funny')
+  gMemes[15].keywords.push('men', 'funny')
+  gMemes[16].keywords.push('men', 'funny')
+  gMemes[17].keywords.push('men', 'funny')
 }
 
 function setSelectedImg(id) {
-    gMeme.selectedImgId = id
-    saveToStorage('selectedMemeDB', gMeme)
+  gMeme.selectedImgId = id
+  saveToStorage('selectedMemeDB', gMeme)
 }
 
 function renderImageToEditor() {
-    gMeme = loadFromStorage('selectedMemeDB')
-    const {selectedImgId, selectedLineIdx} = gMeme
+  gMeme = loadFromStorage('selectedMemeDB')
+  const { selectedImgId, selectedLineIdx } = gMeme
 
-    const img = new Image()
-    img.src = `meme-img/${selectedImgId}.jpg`
-    img.onload = () => {
-      const scaleFactor = Math.min(
-        gElCanvas.width / img.width,
-        gElCanvas.height / img.height
-      )
-      const scaledWidth = img.width * scaleFactor
-      const scaledHeight = img.height * scaleFactor
-      gCtx.drawImage(img, 0, 0, scaledWidth, scaledHeight)
-    }
+  const img = new Image()
+  img.src = `meme-img/${selectedImgId}.jpg`
+  img.onload = () => {
+    const scaleFactor = Math.min(
+      gElCanvas.width / img.width,
+      gElCanvas.height / img.height
+    )
+    const scaledWidth = img.width * scaleFactor
+    const scaledHeight = img.height * scaleFactor
+    gCtx.drawImage(img, 0, 0, scaledWidth, scaledHeight)
+    renderMeme()
   }
+}
+
+function getLineText() {
+    return gMeme
+}
+
+function setLineText(txtInput) {
+    gMeme.lines[0].txt = txtInput
+    saveToStorage('selectedMemeDB',gMeme)
+    renderMeme()
+}
