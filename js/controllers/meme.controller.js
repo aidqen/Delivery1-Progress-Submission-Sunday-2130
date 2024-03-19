22be89b3bc7915e670303aa861a8d3b3763162a0
aidqen <idanmarkin8@gmail.com>
2024-03-19 19:21:08 +0200

    Add media query pages

--- js/controllers/meme.controller.js
+++ js/controllers/meme.controller.js
@@ -16,7 +16,6 @@ function renderImages() {
   var memes = getMemes()
   var strHTML = ''
   memes.map(meme => {
-    console.log(meme.id)
     strHTML += `<img src="meme-img/${meme.id}.jpg" class="meme-img" onclick="selectImg(${meme.id})"></img>`
   })
   elContainer.innerHTML = strHTML
