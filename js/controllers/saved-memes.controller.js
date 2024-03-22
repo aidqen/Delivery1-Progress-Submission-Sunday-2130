305c66f794a22664b0924edd6b4abb79c1f8c67d
aidqen <idanmarkin8@gmail.com>
2024-03-22 16:31:01 +0200

    Start saved memes page

--- js/controllers/saved-memes.controller.js
+++ js/controllers/saved-memes.controller.js
@@ -1,1 +1,49 @@
 'use strict'
+
+var gElBody
+
+function onInit() {
+  gElBody = document.querySelector('body')
+  renderStorage('gSavedMemes')
+  renderSavedMemes()
+}
+
+function toggleMenu() {
+  document.querySelector('body').classList.toggle('menu-open')
+}
+
+function renderSavedMemes() {
+  const elMemesContainer = gElBody.querySelector('.memes-container')
+  const savedMemes = getSavedMemes()
+  console.log(savedMemes)
+
+  for (var i = 0 ; i < savedMemes.length ; i++) {
+      elMemesContainer.innerHTML += `<canvas class="meme-${i}" onclick="goToMemeEditor(${i})" height="334px" width="334px"></canvas>`
+      loadCanvasContent(i)
+  }
+}
+
+function loadCanvasContent(idx) {
+  const currMeme = getSavedMemes()[idx]
+    //   console.log(getSavedMemes())
+  const elCanvas = gElBody.querySelector(`canvas.meme-${idx}`)
+  console.log(idx);
+  const ctx = elCanvas.getContext('2d')
+  renderSavedImage(ctx, elCanvas, currMeme)
+}
+
+function renderSavedImage(ctx, elCanvas, meme) {
+  const { selectedImgId } = meme
+  const img = new Image()
+  img.src = `meme-img/${selectedImgId}.jpg`
+  img.onload = () => {
+    const scaleFactor = Math.min(
+      elCanvas.width / img.width,
+      elCanvas.height / img.height
+    )
+    const scaledWidth = img.width * scaleFactor
+    const scaledHeight = img.height * scaleFactor
+    ctx.drawImage(img, 0, 0, scaledWidth, scaledHeight)
+  }
+}
+function goToMemeEditor(idx) {}
---
+++ js/services/saved-memes.service.js
@@ -0,0 +1,31 @@
+'use strict'
+
+var gSavedMemes = [
+  {
+    selectedImgId: 0,
+    selectedLineIdx: 0,
+    lines: [
+      {
+        x: 50,
+        y: 300,
+        txt: 'some random text',
+        size: 30,
+        color: 'white',
+        font: 'Impact',
+        fontStyle: ['normal'],
+        isDrag: false,
+      },
+    ],
+  },
+]
+
+function getSavedMemes() {
+  return gSavedMemes
+}
+
+function saveMeme() {
+  console.log(gMeme)
+  console.log(gSavedMemes)
+  gSavedMemes.push(gMeme)
+  saveToStorage('savedMemesDB', gSavedMemes)
+}
